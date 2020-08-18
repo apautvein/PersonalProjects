e986cfc2cc13737c681cc7ce2ea9ff8ec3a7ef9c
apautvein <apautvein@gmail.com>
2020-08-18 14:01:57 -0500

    Celsius to Fahrenheit

---
+++ CelsiusToF/app.js
@@ -0,0 +1,27 @@
+const conversion = document.querySelector('#convert')
+const celsiusInput = document.querySelector('#input-celsius');
+const submitBtn = document.querySelector('.submit-btn');
+const message = document.querySelector('.message');
+
+
+submitBtn.addEventListener('click', function () {
+    let submit = parseInt(celsiusInput.value);
+
+    if (isNaN(submit)) {
+        setMessage('Please enter a valid number', 'red');
+    } else {
+        convertToF(submit);
+    }
+});
+
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
+
+function convertToF(celsius) {
+    let fahrenheit = celsius * 9 / 5 + 32;
+    console.log(fahrenheit);
+    setMessage(`${fahrenheit} degrees Fahrenheit`, 'green');
+}
